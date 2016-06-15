d7f7ac646a81676339f91c4334270ec533ae956f
Thomas Kerpe <thomas.kerpe@1und1.de>
2016-06-15 19:06:21 +0200

    Added magic for "Autodetectable Binary Upload™"

--- dht11node/src/TemperatureSensorDHT22.ino
+++ dht11node/src/TemperatureSensorDHT22.ino
@@ -1,6 +1,13 @@
 #include <Homie.h>
 #include "DHT.h"
 
+#define FW_NAME "dht-node"
+#define FW_VERSION "1.0.3"
+
+/* Magic sequence for Autodetectable Binary Upload */
+const char *__FLAGGED_FW_NAME = "\xbf\x84\xe4\x13\x54" FW_NAME "\x93\x44\x6b\xa7\x75";
+const char *__FLAGGED_FW_VERSION = "\x6a\x3f\x3e\x0e\xe1" FW_VERSION "\xb0\x30\x48\xd4\x1a";
+
 #define DHTPIN 14
 
 // Uncomment whatever type you're using!
@@ -60,7 +67,7 @@ void loopHandler() {
 
 void setup() {
   dht.begin();
-  Homie.setFirmware("dht-node", "1.0.1");
+  Homie.setFirmware(FW_NAME, FW_VERSION);
   Homie.registerNode(temperatureNode);
   Homie.registerNode(heatindexNode);
   Homie.registerNode(humidityNode);
