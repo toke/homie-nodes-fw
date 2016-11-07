d225b4200a11023b2f6a63fc96f52c6a4dcdeb8b
Thomas Kerpe <thomas.kerpe@1und1.de>
2016-11-07 17:31:48 +0100

    Use unified sensors library

--- dht11node/src/TemperatureSensorDHT22.ino
+++ dht11node/src/TemperatureSensorDHT22.ino
@@ -1,5 +1,7 @@
 #include <Homie.h>
 #include "DHT.h"
+#include "DHT_U.h"
+
 
 #define FW_NAME "dht-node"
 #define FW_VERSION "1.0.7"
@@ -8,14 +10,14 @@
 const char *__FLAGGED_FW_NAME = "\xbf\x84\xe4\x13\x54" FW_NAME "\x93\x44\x6b\xa7\x75";
 const char *__FLAGGED_FW_VERSION = "\x6a\x3f\x3e\x0e\xe1" FW_VERSION "\xb0\x30\x48\xd4\x1a";
 
-#define DHTPIN 14
+#define DHTPIN D4
 
 // Uncomment whatever type you're using!
-//#define DHTTYPE DHT11   // DHT 11
-#define DHTTYPE DHT22   // DHT 22  (AM2302), AM2321
+#define DHTTYPE DHT11   // DHT 11
+//#define DHTTYPE DHT22   // DHT 22  (AM2302), AM2321
 //#define DHTTYPE DHT21   // DHT 21 (AM2301)
 
-const int TEMPERATURE_INTERVAL = 300;
+const int TEMPERATURE_INTERVAL = 60;
 
 unsigned long lastTemperatureSent = 0;
 
