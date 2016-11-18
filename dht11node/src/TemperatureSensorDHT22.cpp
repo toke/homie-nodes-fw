3ba0339cb5a5190efa36e60e1d73c5ddaced2311
Thomas Kerpe <thomas.kerpe@1und1.de>
2016-11-18 22:45:21 +0100

    Added Button support

--- dht11node/src/TemperatureSensorDHT22.cpp
+++ dht11node/src/TemperatureSensorDHT22.cpp
@@ -5,9 +5,10 @@
 
 
 #define FW_NAME "dht-node"
-#define FW_VERSION "2.0.0"
+#define FW_VERSION "2.0.1"
 
 #define DHTPIN D4
+#define PIN_BUTTON1 0
 
 // Uncomment whatever type you're using!
 //#define DHTTYPE DHT11   // DHT 11
@@ -21,11 +22,18 @@ unsigned long lastTemperatureSent = 0;
 
 void setupHandler();
 void loopHandler();
+bool broadcastHandler(const String& level, const String& value);
 
+Bounce debouncer = Bounce(); // Bounce is built into Homie, so you can use it without including it first
+
+
+int lastButton1Value = -1;
 
 HomieNode temperatureNode("temperature", "temperature");
 HomieNode heatindexNode("heatindex", "temperature");
 HomieNode humidityNode("humidity", "humidity");
+HomieNode buttonNode1("button1", "button");
+
 DHT dht(DHTPIN, DHTTYPE);
 
 void setupHandler() {
@@ -36,6 +44,16 @@ void setupHandler() {
 }
 
 void loopHandler() {
+  int buttonValue = debouncer.read();
+
+  if (buttonValue != lastButton1Value) {
+     Serial << "Button is now " << (buttonValue ? "open" : "close") << endl;
+
+     buttonNode1.setProperty("open").send(buttonValue ? "true" : "false");
+     lastButton1Value = buttonValue;
+  }
+
+
   if (millis() - lastTemperatureSent >= TEMPERATURE_INTERVAL * 1000UL || lastTemperatureSent == 0) {
     float temperature = dht.readTemperature();
     float humidity = dht.readHumidity();
@@ -70,6 +88,10 @@ void loopHandler() {
   }
 }
 
+bool broadcastHandler(const String& level, const String& value) {
+  Serial << "Received broadcast level " << level << ": " << value << endl;
+  return true;
+}
 
 void setup() {
   Serial.begin(115200);
@@ -78,16 +100,25 @@ void setup() {
   Homie_setFirmware(FW_NAME, FW_VERSION);
   Homie.setSetupFunction(setupHandler).setLoopFunction(loopHandler);
 
+  pinMode(PIN_BUTTON1, INPUT);
+  digitalWrite(PIN_BUTTON1, HIGH);
+  debouncer.attach(PIN_BUTTON1);
+  debouncer.interval(50);
+
+
   temperatureNode.advertise("unit");
   temperatureNode.advertise("degrees");
   heatindexNode.advertise("unit");
   heatindexNode.advertise("degrees");
   humidityNode.advertise("unit");
   humidityNode.advertise("percent");
+  buttonNode1.advertise("open");
 
+  Homie.setBroadcastHandler(broadcastHandler); // before Homie.setup()
   Homie.setup();
 }
 
 void loop() {
   Homie.loop();
+  debouncer.update();
 }
