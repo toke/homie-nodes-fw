afd7eb19370d5e98fb2bf8209c59a5fde88a9a46
Thomas Kerpe <thomas.kerpe@1und1.de>
2016-11-21 12:00:53 +0100

    Moved to homie 2.0 (Mostly the esp8266 example)

--- sonoff_node/src/homie-sonoff01.ino
+++ sonoff_node/src/homie-sonoff01.ino
@@ -1,4 +1,9 @@
-/* WARNING: untested */
+/*
+ *   Tested with "WiFi Smart Socket ESP8266 MQTT"
+ *   and "Sonoff - WiFi Wireless Smart Switch ESP8266 MQTT"
+ *
+ *   The Relay could be toggeled with the physical pushbutton
+*/
 
 #include <Homie.h>
 
@@ -6,16 +11,20 @@ const int PIN_RELAY = 12;
 const int PIN_LED = 13;
 const int PIN_BUTTON = 0;
 
+unsigned long buttonDownTime = 0;
+byte lastButtonState = 1;
+byte buttonPressHandled = 0;
+
 HomieNode switchNode("switch", "switch");
 
-bool switchOnHandler(String value) {
+bool switchOnHandler(HomieRange range, String value) {
   if (value == "true") {
     digitalWrite(PIN_RELAY, HIGH);
-    Homie.setNodeProperty(switchNode, "on", "true");
+    switchNode.setProperty("on").send("true");
     Serial.println("Switch is on");
   } else if (value == "false") {
     digitalWrite(PIN_RELAY, LOW);
-    Homie.setNodeProperty(switchNode, "on", "false");
+    switchNode.setProperty("on").send("false");
     Serial.println("Switch is off");
   } else {
     return false;
@@ -24,18 +33,53 @@ bool switchOnHandler(String value) {
   return true;
 }
 
+void toggleRelay() {
+  if (digitalRead(PIN_RELAY) == LOW) {
+    digitalWrite(PIN_RELAY, HIGH);
+    switchNode.setProperty("on").send("true");
+    Serial.println("Switch is on");
+  } else {
+    digitalWrite(PIN_RELAY, LOW);
+    switchNode.setProperty("on").send("false");
+    Serial.println("Switch is off");
+  }
+}
+
+void loopHandler() {
+  byte buttonState = digitalRead(PIN_BUTTON);
+  if ( buttonState != lastButtonState ) {
+    if (buttonState == LOW) {
+      buttonDownTime     = millis();
+      buttonPressHandled = 0;
+    }
+    else {
+      unsigned long dt = millis() - buttonDownTime;
+      if ( dt >= 90 && dt <= 900 && buttonPressHandled == 0 ) {
+        toggleRelay();
+        buttonPressHandled = 1;
+      }
+    }
+    lastButtonState = buttonState;
+  }
+}
+
 void setup() {
+  Serial.begin(115200);
+  Serial.println();
+  Serial.println();
   pinMode(PIN_RELAY, OUTPUT);
+  pinMode(PIN_BUTTON, INPUT);
   digitalWrite(PIN_RELAY, LOW);
 
-  Homie.setFirmware("itead-sonoff", "1.0.0");
-  Homie.setLedPin(PIN_LED, LOW);
-  Homie.setResetTrigger(PIN_BUTTON, LOW, 5000);
-  switchNode.subscribe("on", switchOnHandler);
-  Homie.registerNode(switchNode);
+  Homie_setFirmware("itead-sonoff-buton", "1.0.0");
+  Homie.setLedPin(PIN_LED, LOW).setResetTrigger(PIN_BUTTON, LOW, 5000);
+
+  switchNode.advertise("on").settable(switchOnHandler);
+
+  Homie.setLoopFunction(loopHandler);
   Homie.setup();
 }
 
 void loop() {
   Homie.loop();
-} 
+}
